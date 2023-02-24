0cb4ed07ab43929acc012eb5c4bb112197deee5a
HunterJA <smokesultan@gmail.com>
2023-02-24 09:33:32 +0600

    init: morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+function decodeNumbToLetter(expr) {
+    let i = 0;
+    let letter = '';
+
+
+    while (expr[i]==='0') {
+        i++;
+    }
+    expr = expr.substring(i);
+
+    for (i=0; i<expr.length; i+=2) {
+        if (expr[i+1] === '0') {
+            letter = letter + '.';
+        } else {
+            letter = letter + '-';
+        }
+    }
+
+    return MORSE_TABLE[letter];
+}
+
+    let result = "";
+
+    for(i=0; i<expr.length; i+=10) {
+        if (expr.slice(i, i+1) === '*') {
+            result = result + ' ';
+        } else {
+            result = result + decodeNumbToLetter(expr.slice(i, i+10));
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
